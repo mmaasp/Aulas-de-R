68a6b8f5d274c83c1624c3f90ad68dafb32cc048
mmaasp <165334207+mmaasp@users.noreply.github.com>
2024-03-28 19:37:38 -0300

    Comando Cat
    Introdução ao R com o comando CAT

---
+++ 01_AULA01/Aula01.r
@@ -0,0 +1,37 @@
+Original file is located at
+    https://colab.research.google.com/drive/145Q6FC1lUv2aWKsS5T9m4VUNm-4DyLsG
+
+Aula 1 Programacao em R
+
+cat ('Ola Mundo')
+"""
+
+cat ('Ola Mundo')
+
+cat ('Ola Mundo')
+
+# Solicitar entrada do usuario para os numeros adicao
+numero1 <- as.numeric(readline("Digite o primeiro numero: "))
+numero2 <- as.numeric(readline("Digite o segundo numero: "))
+resultado <- numero1 + numero2
+cat("A soma de", numero1, "e", numero2, "é: ", resultado)
+
+cat(resultado)
+
+# Solicitar entrada do usuario para os numeros subtracao
+numero1 <- as.numeric(readline("Digite o primeiro numero: "))
+numero2 <- as.numeric(readline("Digite o segundo numero: "))
+resultado <- numero1 - numero2
+cat("A subtracao de", numero1, "e", numero2, "é: ", resultado)
+
+# Solicitar entrada do usuario para os numeros multiplicacao
+numero1 <- as.numeric(readline("Digite o primeiro numero: "))
+numero2 <- as.numeric(readline("Digite o segundo numero: "))
+resultado <- numero1 * numero2
+cat("A multiplicacao de", numero1, "e", numero2, "é: ", resultado)
+
+# Solicitar entrada do usuario para os numeros divisao
+numero1 <- as.numeric(readline("Digite o primeiro numero: "))
+numero2 <- as.numeric(readline("Digite o segundo numero: "))
+resultado <- numero1 / numero2
+cat("A divisao de", numero1, "e", numero2, "é: ", resultado)
